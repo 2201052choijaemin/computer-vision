2332827ebbb7709b96c25cc6ea784afe6cb124a5
jaemin <choijamin0405@gmail.com>
2026-05-18 13:05:02 +0900

    Update filter mask for image processing

--- chap7-1/project1/1.cpp
+++ chap7-1/project1/1.cpp
@@ -2,19 +2,24 @@
 #include <opencv2/opencv.hpp>
 using namespace cv;
 using namespace std;
+
 int main(void)
 {
     Mat src = imread("rose.bmp", IMREAD_GRAYSCALE);
+
     if (src.empty()) {
         cerr << "Image load error!" << endl;
         return -1;
     }
-    float data[] = {-1, -1,  0,-1,  0,  1, 0,  1,  1};
-    Mat emboss(3, 3, CV_32FC1, data);
+    float data[] = {1/9.f,1 / 9.f, 1 / 9.f, 1 / 9.f, 1 / 9.f, 1 / 9.f, 1 / 9.f, 1 / 9.f, 1 / 9.f};
+
+    Mat mask(3, 3, CV_32FC1, data);
     Mat dst;
-    filter2D(src, dst, -1, emboss, Point(-1, -1), 128);
+    filter2D(src, dst, -1, mask, Point(-1, -1), 0);
+
     imshow("src", src);
     imshow("dst", dst);
+
     waitKey(0);
     return 0;
 }
